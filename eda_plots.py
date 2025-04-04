d0e7d01a3c08f3bab17a3a1e3dd1616f109ec315
capac <capac@users.noreply.github.com>
2025-04-04 18:07:27 +0100

    New style sheet and added logarithmic price histogram

--- eda_plots.py
+++ eda_plots.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 # working directories
 home_dir = Path.home()
@@ -18,7 +19,7 @@ path_plots = inside_airbnb_work_dir / 'plots'
 path_plots.mkdir(exist_ok=True, parents=True)
 
 # matplotlib style file
-mplstyle_file = inside_airbnb_data_dir / 'barplot-style.mplstyle'
+mplstyle_file = inside_airbnb_work_dir / 'barplot-style.mplstyle'
 plt.style.use(mplstyle_file)
 
 # drop unnecessary features
@@ -38,14 +39,26 @@ if not plot_filename.exists():
     plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
 
-# histogram of price distribution plot
+# histogram plot of price distribution
 prices_data = inside_airbnb_df.price.values
 prices_data = prices_data.astype('float')
 fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.hist(prices_data, bins=50, range=[100, 1000])
 ax.set_title('Histogram of price distribution')
-ax.set_xlabel('Price per night')
+ax.set_xlabel('Price per night (in GBP)')
 plot_filename = path_plots / 'hist_price_distrib.png'
+if not plot_filename.exists():
+    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+
+
+# histogram plot of logarithmic price distribution
+prices_data = np.log1p(inside_airbnb_df.price.values)
+prices_data = prices_data.astype('float')
+fig, ax = plt.subplots(nrows=1, ncols=1)
+ax.hist(prices_data, bins=50)
+ax.set_title('Histogram of logarithmic price distribution')
+ax.set_xlabel('Price per night (in logarithm of GBP)')
+plot_filename = path_plots / 'hist_log_price_distrib.png'
 if not plot_filename.exists():
     plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
@@ -69,7 +82,7 @@ average_price_per_borough_sr.sort_values(ascending=False, inplace=True)
 fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.bar(average_price_per_borough_sr.index, average_price_per_borough_sr.values)
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
-ax.set_ylabel('Average price per night')
+ax.set_ylabel('Average price per night (in GBP)')
 ax.set_title('Average price per night per borough')
 plot_filename = path_plots / 'average_price_borough.png'
 if not plot_filename.exists():
