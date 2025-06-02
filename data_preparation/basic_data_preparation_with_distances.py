bcf36302fa9ac2be235f5f706e3e395f6b6cbf63
capac <capac@users.noreply.github.com>
2025-06-02 12:46:35 +0100

    Rounded both distance to nearest Tube station and geographical coordinates

--- data_preparation/basic_data_preparation_with_distances.py
+++ data_preparation/basic_data_preparation_with_distances.py
@@ -52,45 +52,42 @@ inside_airbnb_df.drop(['calendar_last_scraped', 'last_review'],
                       axis=1, inplace=True)
 
 print('Retaining only properties reviewed within the last six months')
-six_months_in_days = 182
+six_months_in_days = 183
 inside_airbnb_df = inside_airbnb_df[
-    (inside_airbnb_df['days_from_last_review'] <
+    (inside_airbnb_df['days_from_last_review'] <=
      six_months_in_days)]
 
-year_minus_three_months_in_days = 275
+year_minus_ninety_days = 275
 print(f'Retaining properties that have been occupied at '
-      f'least {365 - year_minus_three_months_in_days} days in the past year')
+      f'least {365 - year_minus_ninety_days} days in the past year')
 inside_airbnb_df = inside_airbnb_df[
-    (inside_airbnb_df.availability_365 < year_minus_three_months_in_days)]
+    (inside_airbnb_df.availability_365 < year_minus_ninety_days)]
 
 limit_num_categories = 30
-print(f'Retaining only properties types present '
+print(f'Retaining only property types present '
       f'{limit_num_categories} times or more')
 inside_airbnb_sr = (inside_airbnb_df
                     .groupby('property_type')['property_type']
                     .count()
                     .sort_values(ascending=False))
 inside_airbnb_sr_30 = inside_airbnb_sr[
-    inside_airbnb_sr.values > limit_num_categories
-    ]
+    inside_airbnb_sr.values >= limit_num_categories]
 inside_airbnb_property_type_list = list(inside_airbnb_sr_30.index)
 
 inside_airbnb_df = inside_airbnb_df.loc[
     (inside_airbnb_df['property_type']
      .isin(inside_airbnb_property_type_list))]
 
-print('Rounding latitude and longitude coordinates to six digits')
+print('Rounding latitude and longitude to six digits')
 inside_airbnb_df[['latitude', 'longitude']] = \
     inside_airbnb_df[['latitude', 'longitude']].apply(
-        lambda col: round(col, 6)
-        )
+        lambda col: round(col, 6))
 
 print('Added crime data per borough')
 inside_airbnb_df = inside_airbnb_df.merge(
-    crime_rate_df, on='borough', how='left'
-    )
+    crime_rate_df, on='borough', how='left')
 
-print('Adding distance to nearest Underground station using TfL API')
+print('Adding distance to nearest Tube station using TfL API')
 response = requests.get(TFL_API_URL)
 if response.status_code == 200:
     data = response.json()
@@ -103,8 +100,7 @@ for stop_point in data["stopPoints"]:
     tube_stations.append((lat, lon))
 
 tube_df = pd.DataFrame(
-    tube_stations, columns=["Latitude", "Longitude"]
-    )
+    tube_stations, columns=["Latitude", "Longitude"])
 tube_coords = np.array(tube_df[['Latitude', 'Longitude']])
 tree = KDTree(tube_coords)
 
@@ -116,16 +112,17 @@ def find_nearest_station(lat, lon):
         (lat, lon),
         (nearest_station.Latitude, nearest_station.Longitude)
         ).kilometers
-    return round(distance, 6)
+    return round(distance, 3)
 
 
 inside_airbnb_df['distance_to_nearest_tube_station'] = \
     inside_airbnb_df.apply(
         lambda row: find_nearest_station(row['latitude'], row['longitude']),
-        axis=1, result_type='expand'
-        )
+        axis=1, result_type='expand')
 
 selected_data_file = (inside_airbnb_data_dir /
                       'selected_short_term_rentals_with_distances.csv')
 if not selected_data_file.exists():
     inside_airbnb_df.to_csv(selected_data_file, index=False)
+
+print('Done!')
