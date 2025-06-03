6c8e9dabd8006c85afdb297a6510099ef80bb945
capac <capac@users.noreply.github.com>
2025-06-03 11:27:50 +0100

    Generated CSV file with separate amenity columns

--- data_preparation/data_preparation_for_modeling.py
+++ data_preparation/data_preparation_for_modeling.py
@@ -3,23 +3,24 @@ import pandas as pd
 import numpy as np
 
 home_dir = Path.home()
-inside_airbnb_data_dir = home_dir / 'Programming/data/inside-airbnb/london'
+inside_airbnb_data_dir = (
+    home_dir / 'Programming/data/inside-airbnb/london')
 
-selected_data_file = (inside_airbnb_data_dir /
-                      'selected_short_term_rentals_with_distances.csv')
+selected_data_file = (
+    inside_airbnb_data_dir /
+    'selected_short_term_rentals_with_distances_and_amenities.csv')
 inside_airbnb_df = pd.read_csv(selected_data_file)
 
-inside_airbnb_df['borough'] = \
-    inside_airbnb_df['borough'].replace({r'\s': r'_'}, regex=True)
-inside_airbnb_df[['amenity_1', 'amenity_2', 'amenity_3']] = \
-    inside_airbnb_df['amenities'].str.split(', ', expand=True)
+inside_airbnb_df[['amenity_1', 'amenity_2', 'amenity_3']] = (
+    inside_airbnb_df['nearest_amenity']
+    .str.split(', ', n=3, expand=True)
+    .reindex(range(3), axis=1)
+    )
 inside_airbnb_df = inside_airbnb_df.replace(np.nan, 'None')
-inside_airbnb_df = inside_airbnb_df.drop('amenities', axis=1)
+inside_airbnb_df = inside_airbnb_df.drop('nearest_amenity', axis=1)
 
-# saving results to CSV file with separate amenity
-# columns and underscores in borough names
-print('Saving CSV file with separate amenity columns '
-      'and underscores in borough names.')
+# saving results to CSV file with separate amenity columns
+print('Saving CSV file with separate amenity columns.')
 selected_data_file = (
     inside_airbnb_data_dir /
     'selected_short_term_rentals_for_modeling.csv')
