eb94abc1b5256a4cc88369471ed340cbef917712
capac <capac@users.noreply.github.com>
2025-06-11 15:17:52 +0100

    EDA plots with new dataset

--- eda_plots.py
+++ eda_plots.py
@@ -4,29 +4,30 @@ import pandas as pd
 import numpy as np
 
 # working directories
-HOME_PATH = Path.home()
-INSIDE_AIRBNB_DATA_PATH = HOME_PATH / 'Programming/data/inside-airbnb/london'
-INSIDE_AIRBNB_WORK_PATH = (HOME_PATH / 'Programming/Python/'
-                           'machine-learning-exercises/'
-                           'short-term-rents-in-london')
-inside_airbnb_data_file = (INSIDE_AIRBNB_DATA_PATH /
-                           'selected_short_term_rentals_with_distances.csv')
+home_dir = Path.home()
+inside_airbnb_data_path = home_dir / 'Programming/data/inside-airbnb/london'
+inside_airbnb_work_path = (
+    home_dir / 'Programming/Python/machine-learning-exercises/'
+    'short-term-rents-in-london')
+inside_airbnb_data_file = (
+    inside_airbnb_data_path /
+    'selected_short_term_rentals_for_modeling.csv')
 inside_airbnb_df = pd.read_csv(inside_airbnb_data_file,
                                keep_default_na=False, thousands=',')
 
 # output direcotry for plots
-path_plots = INSIDE_AIRBNB_WORK_PATH / 'plots' / 'histograms'
+path_plots = inside_airbnb_work_path / 'plots' / 'histograms'
 path_plots.mkdir(exist_ok=True, parents=True)
 
 # matplotlib style file
-mplstyle_file = INSIDE_AIRBNB_WORK_PATH / 'barplot-style.mplstyle'
+mplstyle_file = inside_airbnb_work_path / 'barplot-style.mplstyle'
 plt.style.use(mplstyle_file)
 
 
 # short term rentals by borough
 borough_sr = inside_airbnb_df.borough.value_counts()
 fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.bar(borough_sr.index, borough_sr.values)
+ax.bar(borough_sr.index, borough_sr.to_numpy())
 ax.set_ylabel('Number of short term rentals')
 ax.set_title('Short term rentals by borough')
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
@@ -39,7 +40,7 @@ if not plot_filename.exists():
 prices_data = inside_airbnb_df.price.values
 prices_data = prices_data.astype('float')
 fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.hist(prices_data, bins=50, range=[100, 1000])
+ax.hist(prices_data, bins=50, range=(100, 1000))
 ax.set_title('Histogram of price distribution')
 ax.set_xlabel('Price per night (in GBP)')
 plot_filename = path_plots / 'hist_price_distrib.png'
@@ -48,7 +49,7 @@ if not plot_filename.exists():
 
 
 # histogram plot of logarithmic price distribution
-prices_data = np.log1p(inside_airbnb_df.price.values)
+prices_data = np.log1p(inside_airbnb_df.price.to_numpy())
 prices_data = prices_data.astype('float')
 fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.hist(prices_data, bins=50)
@@ -59,22 +60,10 @@ if not plot_filename.exists():
     plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
 
-# minimum number of nights data distribution
-min_nights_data = inside_airbnb_df.minimum_nights.values
-min_nights_data = min_nights_data.astype('int')
-fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.hist(min_nights_data, bins=50, range=[1, 400])
-ax.set_title('Histogram of minimum number of nights data distribution')
-ax.set_xlabel('Minimum number of nights')
-plot_filename = path_plots / 'min_nights_data_distrib.png'
-if not plot_filename.exists():
-    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
-
-
 # number of units by property types plot
 property_type_sr = inside_airbnb_df.property_type.value_counts()
 fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.bar(x=property_type_sr.index, height=property_type_sr.values)
+ax.bar(x=property_type_sr.index, height=property_type_sr.to_numpy())
 ax.set_ylabel('Number of units')
 ax.set_title('Number of units by property types')
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
@@ -88,7 +77,8 @@ average_price_per_borough_sr = inside_airbnb_df.price.groupby(
     inside_airbnb_df.borough).mean()
 average_price_per_borough_sr.sort_values(ascending=False, inplace=True)
 fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.bar(average_price_per_borough_sr.index, average_price_per_borough_sr.values)
+ax.bar(average_price_per_borough_sr.index,
+       average_price_per_borough_sr.to_numpy())
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
 ax.set_ylabel('Average price per night (in GBP)')
 ax.set_title('Average price per night per borough')
@@ -102,7 +92,10 @@ inside_airbnb_num_df = inside_airbnb_df.select_dtypes(include=[np.number])
 fig, axs = plt.subplots(nrows=4, ncols=2, figsize=(13, 10))
 num_columns = inside_airbnb_num_df.columns
 for ax, col in zip(axs.flatten(), num_columns):
-    ax.hist(inside_airbnb_num_df[col], bins=50)
+    if col == 'price':
+        ax.hist(inside_airbnb_num_df[col], bins=50, range=(100, 1000))
+    else:
+        ax.hist(inside_airbnb_num_df[col], bins=50)
     ax.set_ylabel(col, fontsize=10)
     ax.set_title(f'Histogram of {col}', fontsize=12)
     ax.yaxis.set_tick_params(pad=3)
