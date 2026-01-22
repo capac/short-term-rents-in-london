732d46f12e4dc4f8e22a70d57f89a5befdadca91
capac <capac@users.noreply.github.com>
2026-01-22 17:35:23 +0000

    Plots for number and median price of rentals for each London borough
    Matplotlib stylesheet not employed due to incorrect colorbar padding

---
+++ london_maps.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from pathlib import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+import geopandas as gpd
+
+# working directories
+home_dir = Path.home()
+data_dir = home_dir / 'Programming/data/inside-airbnb/london'
+inside_airbnb_data_dir = data_dir / '2024-12-11'
+
+work_dir = home_dir / 'Programming/Python/machine-learning-exercises/'
+inside_airbnb_work_dir = work_dir / 'short-term-rents-in-london'
+
+# output direcotry for plots
+plot_dir = inside_airbnb_work_dir / 'plots' / 'maps'
+plot_dir.mkdir(exist_ok=True, parents=True)
+
+inside_airbnb_data_file = (
+    inside_airbnb_data_dir / 'selected_short_term_rentals_for_modeling.csv'
+    )
+inside_airbnb_df = pd.read_csv(inside_airbnb_data_file,
+                               keep_default_na=False, thousands=',')
+
+# dataframe for median prices of airbnb listings
+median_price_df = inside_airbnb_df['price'].groupby(
+    inside_airbnb_df['borough']).median()
+
+# dataframe for number of airbnb listings
+number_listings_df = pd.DataFrame(inside_airbnb_df.groupby(
+    inside_airbnb_df['borough']).size())
+number_listings_df.rename(columns={0: 'number_listings'}, inplace=True)
+
+neighbourhoods_file = inside_airbnb_data_dir / 'neighbourhoods.geojson'
+map_df = gpd.read_file(neighbourhoods_file)
+map_df.drop('neighbourhood_group', axis=1, inplace=True)
+map_df.rename(columns={'neighbourhood': 'borough'}, inplace=True)
+
+# median price and number of listings joined dataframe
+final_df = map_df.set_index('borough').join(
+    [median_price_df, number_listings_df])
+
+
+# Plotting the number of listings in each borough
+fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+final_df.plot(
+    column='number_listings', cmap='coolwarm', ax=ax
+    )
+ax.set_title('Number of AirBNB rentals for each London borough', fontsize=14)
+sm = plt.cm.ScalarMappable(
+    cmap='coolwarm', norm=plt.Normalize(
+        vmin=min(final_df.number_listings),
+        vmax=max(final_df.number_listings)
+        )
+    )
+ax.axis('off')
+cbar = fig.colorbar(sm, ax=ax)
+plot_filename = plot_dir / 'number-rentals-per-borough.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+
+
+# Plotting the median price of listings in each borough
+fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+final_df.plot(column='price', cmap='coolwarm', ax=ax)
+ax.set_title('Median price of AirBNB rentals for each London borough',
+             fontsize=14)
+sm = plt.cm.ScalarMappable(
+    cmap='coolwarm', norm=plt.Normalize(
+        vmin=min(final_df.price),
+        vmax=max(final_df.price)
+        )
+    )
+ax.axis('off')
+cbar = plt.colorbar(sm, ax=ax)
+plot_filename = plot_dir / 'median-price-per-borough.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
