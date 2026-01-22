81fb944bb0c357e2fb9cf8ec3af311c5e75f95a9
capac <capac@users.noreply.github.com>
2026-01-22 17:20:02 +0000

    Changed plot path name to 'plot_dir' and removed check for existing plots
    Existing plots will be overwritten

--- eda_plots.py
+++ eda_plots.py
@@ -19,8 +19,8 @@ inside_airbnb_df = pd.read_csv(inside_airbnb_data_file,
                                keep_default_na=False, thousands=',')
 
 # output direcotry for plots
-path_plots = inside_airbnb_work_dir / 'plots' / 'histograms'
-path_plots.mkdir(exist_ok=True, parents=True)
+plot_dir = inside_airbnb_work_dir / 'plots' / 'histograms'
+plot_dir.mkdir(exist_ok=True, parents=True)
 
 # matplotlib style file
 mplstyle_file = inside_airbnb_work_dir / 'barplot-style.mplstyle'
@@ -34,9 +34,8 @@ ax.bar(borough_sr.index, borough_sr.to_numpy())
 ax.set_ylabel('Number of short term rentals')
 ax.set_title('Short term rentals by borough')
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
-plot_filename = path_plots / 'short-term-rentals.png'
-if not plot_filename.exists():
-    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+plot_filename = plot_dir / 'short-term-rentals.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
 
 # histogram plot of price distribution
@@ -46,9 +45,8 @@ fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.hist(prices_data, bins=50, range=(100, 1000))
 ax.set_title('Histogram of price distribution')
 ax.set_xlabel('Price per night (in GBP)')
-plot_filename = path_plots / 'hist_price_distrib.png'
-if not plot_filename.exists():
-    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+plot_filename = plot_dir / 'hist_price_distrib.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
 
 # histogram plot of logarithmic price distribution
@@ -58,9 +56,8 @@ fig, ax = plt.subplots(nrows=1, ncols=1)
 ax.hist(prices_data, bins=50)
 ax.set_title('Histogram of logarithmic price distribution')
 ax.set_xlabel('Price per night (in logarithm of GBP)')
-plot_filename = path_plots / 'hist_log_price_distrib.png'
-if not plot_filename.exists():
-    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+plot_filename = plot_dir / 'hist_log_price_distrib.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
 
 # number of units by property types plot
@@ -70,9 +67,8 @@ ax.bar(x=property_type_sr.index, height=property_type_sr.to_numpy())
 ax.set_ylabel('Number of units')
 ax.set_title('Number of units by property types')
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
-plot_filename = path_plots / 'num_units_property.png'
-if not plot_filename.exists():
-    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+plot_filename = plot_dir / 'num_units_property.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
 
 # average price per night per borough plot
@@ -85,9 +81,8 @@ ax.bar(average_price_per_borough_sr.index,
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
 ax.set_ylabel('Average price per night (in GBP)')
 ax.set_title('Average price per night per borough')
-plot_filename = path_plots / 'average_price_borough.png'
-if not plot_filename.exists():
-    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+plot_filename = plot_dir / 'average_price_borough.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
 
 
 # histogram plot of attributes
@@ -103,6 +98,5 @@ for ax, col in zip(axs.flatten(), num_columns):
     ax.set_title(f'Histogram of {col}', fontsize=12)
     ax.yaxis.set_tick_params(pad=3)
     plt.tight_layout(pad=1)
-plot_filename = path_plots / 'attribute_histogram_plots.png'
-if not plot_filename.exists():
-    plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
+plot_filename = plot_dir / 'attribute_histogram_plots.png'
+plt.savefig(plot_filename, dpi=144, bbox_inches='tight')
