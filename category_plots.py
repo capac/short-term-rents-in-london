937ffb09d27c145846a272b792127a0719cfb5c1
capac <capac@users.noreply.github.com>
2026-03-01 10:03:57 +0000

    Using precise 95% confidence-interval width in plot error bars
    Using 97.5% quantile of a t-distribution with n−2 degrees of freedom correction, also adjusted legend box position in plot

--- category_plots.py
+++ category_plots.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import t, ttest_ind, sem
 
 # working directories
 home_dir = Path.home()
@@ -42,18 +42,22 @@ for category, ax in zip(categories, axes.flatten()):
     filter_with_cat = inside_airbnb_df.loc[:, category] == 1
     price_with_cat = inside_airbnb_df['price'].loc[filter_with_cat]
     price_with_cat_mean = np.mean(price_with_cat)
-    price_with_cat_std = np.std(price_with_cat, ddof=1)
+    price_with_cat_stderr = sem(price_with_cat, ddof=1)
+    # 97.5% quantile of a t-distribution with n−2 degrees of freedom correction
+    correction_with_cat = t.ppf(0.975, len(price_with_cat)-2)
     # data without category feature
     filter_without_cat = inside_airbnb_df.loc[:, category] == 0
     price_without_cat = inside_airbnb_df['price'].loc[filter_without_cat]
     price_without_cat_mean = np.mean(price_without_cat)
-    price_without_cat_std = np.std(price_without_cat, ddof=1)
-    print(f'With category: mean ± std: '
-          f'{np.round(price_with_cat_mean, 3)}±'
-          f'{np.round(price_with_cat_std, 3):<10}'
-          f'Without category: mean ± std: '
-          f'{np.round(price_without_cat_mean, 3)}±'
-          f'{np.round(price_without_cat_std, 3)}')
+    price_without_cat_stderr = sem(price_without_cat, ddof=1)
+    # 97.5% quantile of a t-distribution with n−2 degrees of freedom correction
+    correction_without_cat = t.ppf(0.975, len(price_without_cat)-2)
+    # print(f'With category: mean ± std: '
+    #       f'{np.round(price_with_cat_mean, 3)} ± '
+    #       f'{np.round(price_with_cat_stderr, 3):<10}'
+    #       f'Without category: mean ± std: '
+    #       f'{np.round(price_without_cat_mean, 3)} ± '
+    #       f'{np.round(price_without_cat_stderr, 3)}')
     # t-test for the means of two independent samples of scores
     tt = ttest_ind(price_with_cat, price_without_cat)
     tt_st = np.round(tt.statistic, 1)
@@ -61,18 +65,21 @@ for category, ax in zip(categories, axes.flatten()):
     ax.bar(
         [label_with, label_without],
         [price_with_cat_mean, price_without_cat_mean],
-        yerr=[price_with_cat_std, price_without_cat_std],
-        width=0.7, alpha=0.7, capsize=3, error_kw={'elinewidth': 0.8},
+        yerr=[
+            correction_with_cat*price_with_cat_stderr,
+            correction_without_cat*price_without_cat_stderr
+            ],
+        width=0.7, alpha=0.7, error_kw={'elinewidth': 1.0},
         label=(f'Statistic: {tt_st}\n$p$-value: {tt_pval}'))
     ax.set_ylabel('Price (£)', fontsize=8)
     plt.setp(ax.get_yticklabels(), fontsize=8)
     ax.tick_params(axis='y', which='major', pad=5)
     ax.yaxis.set_minor_locator(AutoMinorLocator(2))
     ax.grid(axis='y', which='minor', linestyle=':')
-    ax.legend(loc='upper right', fontsize=4,
-              handlelength=0, handletextpad=0,
-              fancybox=True)
-    ax.set_ylim([0, 430])
+    ax.legend(loc='upper right', fontsize=5,
+              handlelength=0, handletextpad=0, frameon=False,
+              bbox_to_anchor=(1.0, 1.2))
+    ax.set_ylim([0, 235])
     plt.setp(
         ax.get_xticklabels(), ha='right',
         rotation_mode='anchor', rotation=45,
@@ -89,4 +96,7 @@ fig.text(x=0.08, y=-0.01,
 fig.suptitle('Property amenity features vs average rent price',
              fontsize=11, fontweight='bold')
 fig.subplots_adjust(top=0.92)
-fig.savefig(plot_dir / 'property_features_vs_average_price.png')
+fig.savefig(
+    plot_dir / 'property_features_vs_average_price.png',
+    bbox_inches='tight', dpi=288
+    )
